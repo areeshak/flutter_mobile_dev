ffe280503e9264741979ed76bfdd42e176703de5
areeshak <98297607+areeshak@users.noreply.github.com>
2022-05-06 22:44:08 +0500

    add google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'Mini Projects/cart9/dishes_list.dart';
 import 'Basic Widgets/state_management_provider11/counter_provider.dart';
 import 'Basic Widgets/state_management_provider11/counter.dart';
 import 'Basic Widgets/firebase_users12.dart';
-import 'Basic Widgets/google_authentication13/signIn.dart';
+import 'Basic Widgets/google_authentication12/signIn.dart';
 
 
 void main() async {
