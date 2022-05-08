a48f082cb22d39fa84b4248eb81a9a13ae0760c9
areeshak <98297607+areeshak@users.noreply.github.com>
2022-05-08 16:51:32 +0500

    change user model structure

--- lib/Architecture/controller/main_provider.dart
+++ lib/Architecture/controller/main_provider.dart
@@ -14,18 +14,18 @@ class MainProvider{                 //controller
                                                                                                          //DATA FROM API CALL
   Future<List<User>>  getUsers() async{                    //function called by UI
 
-
-
     //var response = await FirebaseApi.getUsers();
     //var list = response.data();                        //data from firebase
     //var userModelList = UserModel.fromJson(response.data());      // json parse in user model || this is user model list but UI needs users list  --> need to convert user model to user
 
-    var userModel = UserModel.fromJson({"name" : "Areesha Kashif"});        //suppose this name is a *json object coming*/api response/ firebase response & we put it in user model
-                //in this user model --> it has json keys
+    var userModel = UserModel.fromJson({                              //entry from database changed to first & last name
+        "firstName" : "Areesha",
+        "LastName" : "Kashif",
+      });
 
                                       //now user model will convert to user object
-    var user = User(name:  userModel.name);
 
+    var user = User(name:  userModel.firstName + ' ' + userModel.lastName);   //need first and last name without changing ui --> concatenate both names and send as one name as to ui it doesn't matter how name is coming
     users.add(user);
     return users;
   }
